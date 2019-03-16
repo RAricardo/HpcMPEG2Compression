133d6393691f3fe3d5ccb5b038e822105a7c3477
Ricardo R. Azopardo <rrazopardc@eafit.edu.co>
2019-03-15 23:53:48 -0500

    Avances en OpenMP

---
+++ OpenMPC++/compresor.cpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include "macrobloque.cpp"
+#include "vector4.cpp"
+#include <omp.h>
+
+using namespace std;
+
+class compresor
+{
+  private:
+    vector<macrobloque>MBArr1;
+    vector<macrobloque> MBArr2;
+
+  public:
+  compresor(){}
+  compresor(vector<macrobloque>MBArr1, vector<macrobloque>MBArr2){
+      this->MBArr1=MBArr1;
+      this->MBArr2=MBArr2;
+  }
+
+    void compresionMPEG2(){
+        
+        for(int i=0;i<MBArr1.size();i++){
+            int valMin=256;
+            vector4 vector;
+            bool compZero = false;
+            #pragma omp for 
+            for(int j=0;j<MBArr2.size();j++){
+                if (compZero) {
+                    compZero = true;
+                } else {
+                    int comparacion = MBArr1[i].comparar(MBArr2[j]);
+                    if(comparacion==0){
+                        vector=vector4(MBArr1[i].x, MBArr1[i].y, MBArr2[j].x, MBArr2[j].y);
+                        compZero = true;
+                    }
+                    else if(comparacion<=valMin){
+                        valMin=comparacion;
+                        vector=vector4(MBArr1[i].x, MBArr1[i].y, MBArr2[j].x, MBArr2[j].y);
+                    }
+                }
+
+            }
+            vector.imprimir();      
+        }
+    }
+};
